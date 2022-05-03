f9412acdb1cb131968a8aad9b51cb5e68c1da323
Christian Pribyl <christianpribyl@protonmail.com>
2022-05-02 23:40:37 -0400

    removed old files

--- c01_prepare_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ./data
-for i in 0 1 2 3 4
-do
-  cp /data/trec-car-2022/enwiki-2022-01-01-package/trainLargePackage/fold-${i}-train.pages.jsonl-splits/pages-1.jsonl.gz ./data/articles-${i}.jsonl.gz
-  gzip -d articles-${i}.jsonl.gz
-  rm ./data/articles-${i}.jsonl.gz
-  ls -l ./data/articles-${i}.jsonl
-done
--- createKnowledgeBase.sh
+++
@@ -1,2 +0,0 @@
-./mvnw clean package
-find ./kb-corpus -name *.jsonl | xargs java -jar target/outlines-1.jar kb-index kb
